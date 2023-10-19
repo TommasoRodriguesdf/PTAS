05d83c5482b70e698f55bd58f39cab44e835302e
TommasoRodriguesdf <dtommasorodrigues@gmail.com>
2023-10-19 16:34:22 -0400

    tomasso

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
+
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index', { message: 'LOJA de ADOSAU de ANIMAIS seuvaseis' });
 });
+
+app.get('/produto', (req, res) => {
+  res.render('produto', { message: "Adote Aqui!"})
+})
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
