b3e0301645b3754607a463eb133fdd145e2eb782
TommasoRodriguesdf <dtommasorodrigues@gmail.com>
2023-10-26 16:49:45 -0400

    animau1.4

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+ const produto=[
+  {id:1, nome:"Foga fofa" , preco:"" , descricao:"", imagem: "foga.png"},
+  {id:2, nome:"Banda vermelho fofo" , preco:"" , descricao:"", imagem: "banda.png"},
+  {id:3, nome:"esquilo fofo" , preco:"" , descricao:"", imagem: "esquilo.png"},
+  {id:4, nome:"" , preco:"" , descricao:"", imagem: ""},
+  {id:5, nome:"" , preco:"" , descricao:"", imagem: ""},
+  {id:6, nome:"Foga fofa" , preco:"" , descricao:"", imagem: "foga.png"},
+  {id:7, nome:"Banda vermelho fofo" , preco:"" , descricao:"", imagem: "banda.png"},
+  {id:8, nome:"esquilo fofo" , preco:"" , descricao:"", imagem: "esquilo.png"},
+  {id:9, nome:"" , preco:"" , descricao:"", imagem: ""},
+  {id:10, nome:"" , preco:"" , descricao:"", imagem: ""},
+ ]
 
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'LOJA de ADOSAU de ANIMAIS seuvaseis' });
+  res.render('index', { message: '' });
 });
 
 app.get('/produto', (req, res) => {
